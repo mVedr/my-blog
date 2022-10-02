6fd4150ad93056bd3a3f31325f012ec2862ec2db
ved2805 <vedrecharla@gmail.com>
2022-10-02 18:57:19 +0530

    second commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  nextConfig : {
+    reactStrictMode: true,
+    swcMinify: true,
+  }
+}
