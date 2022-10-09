7f43315f888241887be19f79790ae1a7667ce129
ved2805 <vedrecharla@gmail.com>
2022-10-09 19:15:13 +0530

    Change background

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Ved&apos;s Blog</span>
+            <span className="cursor-pointer font-bold text-4xl text-white">Ved&#39;s personal space !</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
